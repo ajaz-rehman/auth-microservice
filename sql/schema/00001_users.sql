e330355ae3db078eb31d0d29f5b739370e3c00f3
Ajaz Rehman <ajazurrehman4@gmail.com>
2024-11-26 09:44:16 +0000

    Update schema

--- sql/schema/00001_users.sql
+++ sql/schema/00001_users.sql
@@ -1,6 +1,8 @@
 -- +goose Up
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     hashed_password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
