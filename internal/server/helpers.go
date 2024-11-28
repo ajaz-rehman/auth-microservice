e4f1b58bc850958124cce57b459332bb847b326a
Ajaz Rehman <ajazurrehman4@gmail.com>
2024-11-28 07:09:30 +0000

    Transform request body

--- internal/server/helpers.go
+++ internal/server/helpers.go
@@ -1,21 +1,34 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"io"
 
+	"github.com/go-playground/mold/v4/modifiers"
 	"github.com/go-playground/validator/v10"
 )
 
-func validateAndGetValue[T any](body io.ReadCloser) (value T, err error) {
+func transformAndValidateBody[T any](body io.ReadCloser) (value T, err error) {
+	// Decode the json body into the value
 	err = json.NewDecoder(body).Decode(&value)
 
 	if err != nil {
 		return
 	}
 
+	// Transform the value
+	transform := modifiers.New()
+
+	err = transform.Struct(context.Background(), &value)
+
+	if err != nil {
+		return
+	}
+
+	// Validate the value
 	validate := validator.New(validator.WithRequiredStructEnabled())
-	err = validate.Struct(value)
+	err = validate.Struct(&value)
 
 	return
 }
--- internal/server/signup.go
+++ internal/server/signup.go
@@ -6,15 +6,15 @@ import (
 )
 
 type SignupRequestBody struct {
-	FirstName string `json:"first_name" validate:"required,alpha,ascii,min=2,max=25"`
-	LastName  string `json:"last_name" validate:"required,alpha,ascii,min=2,max=25"`
-	Email     string `json:"email" validate:"required,email"`
-	Password  string `json:"password" validate:"required,ascii,min=8,max=50"`
+	FirstName string `json:"first_name" mod:"trim,lcase,title" validate:"required,alpha,ascii,min=2,max=25"`
+	LastName  string `json:"last_name" mod:"trim,lcase,title" validate:"required,alpha,ascii,min=2,max=25"`
+	Email     string `json:"email" mod:"trim,lcase" validate:"required,email"`
+	Password  string `json:"password" validate:"required,ascii,min=8,max=50,excludes= "`
 }
 
 func signupHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	data, err := validateAndGetValue[SignupRequestBody](r.Body)
+	data, err := transformAndValidateBody[SignupRequestBody](r.Body)
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
